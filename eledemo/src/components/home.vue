c5881dc8a40a2d0289e5d75c7b6bef6fe6759ade
Chenbozhiaaaaaa <15176672280@163.com>
2020-07-08 21:44:58 +0800

    1

--- eledemo/src/components/home.vue
+++ eledemo/src/components/home.vue
@@ -19,7 +19,7 @@
           <el-col :span="8">
             <div class="grid-content">
               <el-breadcrumb separator="/" style="line-height: inherit;">
-                <el-breadcrumb-item v-for="item in levelList" :key="item.path">{{item.name}}</el-breadcrumb-item>
+                <el-breadcrumb-item :to="{ path: item.path }" v-for="item in levelList" :key="item.path">{{item.name}}</el-breadcrumb-item>
               </el-breadcrumb>
             </div>
           </el-col>
